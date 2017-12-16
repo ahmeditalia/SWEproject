5f59287a703b23c22cd108c28a5fdb51c88fd878
obada <obada@gethub.com>
2017-12-16 13:33:29 -0800

    obada

---
+++ SWE2/src/src/AdminController.java
@@ -0,0 +1,23 @@
+package src;
+
+import java.util.*;
+
+/**
+ * 
+ */
+public class AdminController {
+
+	private IDataBase iDataBase = new DataBase();
+
+	public AdminController() {
+	}
+
+	public void AddProductToSystem(String productName, String category, String brand) {
+		Product newP = new Product(productName, category, brand);
+		iDataBase.InsertProductToSystem(newP);
+	}
+
+	public List<Product> getSuggestedProducts() {
+		return iDataBase.RetreiveSysProducts();
+	}
+}
--- SWE2/src/src/Adminform.java
+++ SWE2/src/src/Adminform.java
@@ -6,6 +6,8 @@ import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JCheckBox;
+import javax.swing.JComboBox;
 
 public class Adminform {
 
@@ -53,8 +55,28 @@ public class Adminform {
 			}
 			
 		});
-		btnAddProductTo.setBounds(120, 44, 224, 23);
+		btnAddProductTo.setBounds(26, 138, 224, 34);
 		frame.getContentPane().add(btnAddProductTo);
+		
+		JButton btnNewButton = new JButton("Add Suggested Products");
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				
+			}
+		});
+		btnNewButton.setBounds(26, 93, 224, 34);
+		frame.getContentPane().add(btnNewButton);
+		
+		JButton btnNewButton_1 = new JButton("Show Suggested Products");
+		btnNewButton_1.setBounds(26, 48, 224, 34);
+		frame.getContentPane().add(btnNewButton_1);
+		
+		JComboBox<String> comboBox = new JComboBox<String>();
+		for (int i = 0; i < crunchifyList.size(); i++) {
+			System.out.println(crunchifyList.get(i));
+		}
+		comboBox.setBounds(26, 183, 224, 34);
+		frame.getContentPane().add(comboBox);
+		
 	}
-
 }
