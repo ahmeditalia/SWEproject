048a07980a1b02827b5730761541fc8ba836244d
obada <obada@gethub.com>
2017-12-15 17:01:31 -0800

    Parameterized constructor

--- SWE2/src/src/Store.java
+++ SWE2/src/src/Store.java
@@ -1,116 +1,92 @@
 package src;
 
-
-
 import java.util.*;
 
-/**
- * 
- */
 public class Store {
 
-    /**
-     * Default constructor
-     */
-    public Store() {
-    }
+	public Store() {
+	}
 
-    /**
-     * 
-     */
-    private String StoreName;
+	public Store(String SName, StoreOwner owner) {
+		this.StoreName = SName;
+		this.Owner = owner;
+	}
 
-    /**
-     * 
-     */
-    private List<Product> StoreProducts;
+	private String StoreName;
+	private List<Product> StoreProducts;
+	private StoreOwner Owner;
+	private Map<Product, User> SoldProducts;
 
-    /**
-     * 
-     */
-    private StoreOwner Owner;
+	public void AddProduct(Product product) {
 
-    /**
-     * 
-     */
-    private Map<Product,User> SoldProducts;
+	}
 
+	/**
+	 * @param product
+	 * @param Buyer
+	 */
+	public void SellProduct(Product product, User Buyer) {
+		// TODO implement here
+	}
 
+	/**
+	 * @return
+	 */
+	public String getStoreName() {
+		// TODO implement here
+		return "";
+	}
 
+	/**
+	 * @param value
+	 */
+	public void setStoreName(String value) {
+		// TODO implement here
+	}
 
-    /**
-     * @param product
-     */
-    public void AddProduct(Product product) {
-        // TODO implement here
-    }
+	/**
+	 * @return
+	 */
+	public List<Product> getStoreProducts() {
+		// TODO implement here
+		return null;
+	}
 
-    /**
-     * @param product 
-     * @param Buyer
-     */
-    public void SellProduct(Product product, User Buyer) {
-        // TODO implement here
-    }
+	/**
+	 * @param value
+	 */
+	public void setStoreProducts(List<Product> value) {
+		// TODO implement here
+	}
 
-    /**
-     * @return
-     */
-    public String getStoreName() {
-        // TODO implement here
-        return "";
-    }
+	/**
+	 * @return
+	 */
+	public StoreOwner getOwner() {
+		// TODO implement here
+		return null;
+	}
 
-    /**
-     * @param value
-     */
-    public void setStoreName(String value) {
-        // TODO implement here
-    }
+	/**
+	 * @param value
+	 */
+	public void setOwner(StoreOwner value) {
+		// TODO implement here
+	}
 
-    /**
-     * @return
-     */
-    public List<Product> getStoreProducts() {
-        // TODO implement here
-        return null;
-    }
+	/**
+	 * @return
+	 */
+	public Map<Product, User> getSoldProducts() {
+		// TODO implement here
+		return null;
+	}
 
-    /**
-     * @param value
-     */
-    public void setStoreProducts(List<Product> value) {
-        // TODO implement here
-    }
-
-    /**
-     * @return
-     */
-    public StoreOwner getOwner() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param value
-     */
-    public void setOwner(StoreOwner value) {
-        // TODO implement here
-    }
-
-    /**
-     * @return
-     */
-    public Map<Product,User> getSoldProducts() {
-        // TODO implement here
-        return null;
-    }
-
-    /**
-     * @param value
-     */
-    public void setSoldProducts(Map<Product,User> value) {
-        // TODO implement here
-    }
+	/**
+	 * @param value
+	 */
+	public void setSoldProducts(Map<Product, User> value) {
+		// TODO implement here
+	}
 
 }
