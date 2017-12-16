3a7abefa5dcedee2786ff4927b26abbc31c2e0e1
MOhamedHDrmi <hdrmim7@gmail.com>
2017-12-16 20:59:38 +0200

    new update

--- SWE2/src/src/HomePage.java
+++ SWE2/src/src/HomePage.java
@@ -181,6 +181,7 @@ public class HomePage {
 		updatetable(model);
 		table.setModel(model);
 		model.setColumnIdentifiers(columnsnames);
+		model.addRow(new Object[] {"sdsa","sdsa","asd","asds","sdsa","sdsa","asd","asds"});
 		JSeparator separator_3 = new JSeparator();
 		separator_3.setBounds(10, 75, 1342, 2);
 		frmHomepage.getContentPane().add(separator_3);
