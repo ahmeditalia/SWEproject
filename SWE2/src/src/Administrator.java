63bb4605c0d53ee0285a8da292b5833632904926
ahmeditalia <34543951+ahmeditalia@users.noreply.github.com>
2017-12-16 15:59:54 +0200

    constructors modify in adminstrator and storewoner

--- SWE2/src/src/Administrator.java
+++ SWE2/src/src/Administrator.java
@@ -20,5 +20,16 @@ public class Administrator extends User implements Serializable{
      */
     public Administrator() {
     }
+    
+    Administrator(String Username,String Password,String E_mail,String PhoneNumber,String Gender,String Address,int Type){
+    	this.Username=Username;
+        this.Password=Password;
+        this.E_mail=E_mail;
+        this.PhoneNumber=PhoneNumber;
+        this.Gender=Gender;
+        this.Address=Address;
+        this.Type=Type;
+    }
+
 
 }
--- SWE2/src/src/StoreOwner.java
+++ SWE2/src/src/StoreOwner.java
@@ -2,18 +2,35 @@ package src;
 
 
 
+import java.io.Serializable;
 import java.util.*;
 
 /**
  * 
  */
-public class StoreOwner extends User {
+public class StoreOwner extends User implements Serializable{
 
     /**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	/**
      * Default constructor
      */
     public StoreOwner() {
     }
+    
+    StoreOwner(String Username,String Password,String E_mail,String PhoneNumber,String Gender,String Address,int Type){
+    	this.Username=Username;
+        this.Password=Password;
+        this.E_mail=E_mail;
+        this.PhoneNumber=PhoneNumber;
+        this.Gender=Gender;
+        this.Address=Address;
+        this.Type=Type;
+    }
+
     
     private List<Store> OwnedStores= new ArrayList<>();
     
