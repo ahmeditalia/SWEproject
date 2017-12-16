c7e7d0cc34a9c5c5711d744051aa3ddae7cd4bdf
obada <obada@gethub.com>
2017-12-16 14:55:15 -0800

    form for admin

--- SWE2/src/src/AdminController.java
+++ SWE2/src/src/AdminController.java
@@ -7,44 +7,15 @@ import java.util.*;
  */
 public class AdminController {
 
-<<<<<<< HEAD
 	private IDataBase iDataBase = new DataBase();
-
-=======
->>>>>>> f00384ddd0eba5bd89871da47341eaf108917d74
 	public AdminController() {
 	}
-
 	public void AddProductToSystem(String productName, String category, String brand) {
 		Product newP = new Product(productName, category, brand);
-<<<<<<< HEAD
 		iDataBase.InsertProductToSystem(newP);
 	}
 
 	public List<Product> getSuggestedProducts() {
 		return iDataBase.RetreiveSysProducts();
 	}
-=======
-
-<<<<<<< HEAD
-	}
-
-	/**
-	 * @return
-	 */
-	public List<Product> getSuggestedProducts() {
-		// TODO implement here
-		return null;
-	}
-
-    public void AddProductToSystem(String productName, String category, String brand) {
-        // TODO implement here
-    }
-    public List<Product> getSuggestedProducts() {
-        // TODO implement here
-        return null;
-    }
->>>>>>> c439e86e104a5a72cba153e7e197d2c9fcf32a38
-
->>>>>>> f00384ddd0eba5bd89871da47341eaf108917d74
 }
--- SWE2/src/src/Adminform.java
+++ SWE2/src/src/Adminform.java
@@ -1,24 +1,29 @@
 package src;
 
 import java.awt.EventQueue;
+import java.awt.*;
 
 import javax.swing.JFrame;
 import javax.swing.JButton;
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.awt.event.ActionEvent;
 import javax.swing.JCheckBox;
 import javax.swing.JComboBox;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
 
 public class Adminform {
 
 	private JFrame frame;
+	private AdminController adminController = new AdminController();
+	private IDataBase iDataBase = new DataBase();
+	private JTextField Category;
+	private JTextField Brand;
+	Adminform(Administrator admin) {
 
-	AdminController adminCntroller= new AdminController();
-	Adminform(Administrator admin){
-		
 	}
-	
-	
+
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
@@ -48,35 +53,53 @@ public class Adminform {
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
+		JComboBox<String> comboBox = new JComboBox<String>();
 		JButton btnAddProductTo = new JButton("Add product to the system ");
 		btnAddProductTo.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
-				//ProductForm pGui= new ProductForm(Adminform.this);
+				// ProductForm pGui= new ProductForm(Adminform.this);
+				String x = comboBox.getSelectedItem().toString();
+				
+				iDataBase.InsertSuggestedProduct(product);
+				
 			}
-			
+
 		});
-		btnAddProductTo.setBounds(26, 138, 224, 34);
+		btnAddProductTo.setBounds(26, 34, 224, 34);
 		frame.getContentPane().add(btnAddProductTo);
-		
+
 		JButton btnNewButton = new JButton("Add Suggested Products");
 		btnNewButton.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 			}
 		});
-		btnNewButton.setBounds(26, 93, 224, 34);
+		btnNewButton.setBounds(131, 253, 224, 34);
 		frame.getContentPane().add(btnNewButton);
-		
-		JButton btnNewButton_1 = new JButton("Show Suggested Products");
-		btnNewButton_1.setBounds(26, 48, 224, 34);
-		frame.getContentPane().add(btnNewButton_1);
-		
-		JComboBox<String> comboBox = new JComboBox<String>();
-		for (int i = 0; i < crunchifyList.size(); i++) {
-			System.out.println(crunchifyList.get(i));
+
+		List<Product> retreiveCategoryProducts = adminController.getSuggestedProducts();
+		for (int i = 0; i < retreiveCategoryProducts.size(); i++) {
+			comboBox.addItem(retreiveCategoryProducts.get(i).getName());
 		}
-		comboBox.setBounds(26, 183, 224, 34);
+		comboBox.setBounds(260, 34, 224, 34);
 		frame.getContentPane().add(comboBox);
 		
+		JLabel lblCategory = new JLabel("Category :");
+		lblCategory.setBounds(41, 90, 66, 21);
+		frame.getContentPane().add(lblCategory);
+		
+		Category = new JTextField();
+		Category.setBounds(108, 90, 86, 20);
+		frame.getContentPane().add(Category);
+		Category.setColumns(10);
+		
+		JLabel lblBrand = new JLabel("Brand :");
+		lblBrand.setBounds(41, 125, 46, 14);
+		frame.getContentPane().add(lblBrand);
+		
+		Brand = new JTextField();
+		Brand.setBounds(108, 122, 86, 20);
+		frame.getContentPane().add(Brand);
+		Brand.setColumns(10);
 	}
 }
