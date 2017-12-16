edcab1c317d7084c7b04fe9ff69514a461c4c971
Sa2a <khaled.elsaka25@gmail.com>
2017-12-16 03:06:30 +0200

    update stores

--- SWE2/src/src/StoreUI.java
+++ SWE2/src/src/StoreUI.java
@@ -13,6 +13,7 @@ import java.awt.event.ActionEvent;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import javax.swing.JComboBox;
+import javax.swing.JCheckBox;
 
 public class StoreUI {
 	private JFrame frame;
@@ -38,8 +39,6 @@ public class StoreUI {
 		});
 	}
 	
-	
-
 	/**
 	 * Create the application.
 	 */
@@ -48,9 +47,9 @@ public class StoreUI {
 	}
 
 	
-	private void initialize() {
+	 void initialize() {
 		frame = new JFrame();
-		frame.setBounds(100, 100, 558, 392);
+		frame.setBounds(100, 100, 514, 317);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		
@@ -68,7 +67,7 @@ public class StoreUI {
 			StoreList.addItem(sotreController.getCurrentUser().getStoresName().get(i));
 		
 		}
-		StoreList.setBounds(320, 59, 119, 22);
+		StoreList.setBounds(320, 59, 143, 22);
 		frame.getContentPane().add(StoreList);
 		JButton btnAddProduct = new JButton("Add product to store");
 		btnAddProduct.addActionListener(new ActionListener() {
@@ -85,24 +84,44 @@ public class StoreUI {
 		btnSugestProduct.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 				ProductForm pForm = new  ProductForm(StoreUI.this,false);
+				frame.setVisible(false);
 			}
 		});
-		btnSugestProduct.setBounds(28, 111, 272, 23);
+		btnSugestProduct.setBounds(28, 206, 272, 23);
 		frame.getContentPane().add(btnSugestProduct);
 		
 		JButton btnOExploreNumber = new JButton("Get number of products views");
-		btnOExploreNumber.setBounds(28, 162, 272, 23);
+		btnOExploreNumber.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				ShowPViews pViews= new ShowPViews(StoreUI.this);
+				
+			}
+		});
+		btnOExploreNumber.setBounds(28, 103, 272, 23);
 		frame.getContentPane().add(btnOExploreNumber);
 		
-		JButton btnOGetThe = new JButton("o\tGet the most viewed product ");
-		btnOGetThe.setBounds(28, 213, 272, 23);
+		JButton btnOGetThe = new JButton("Get the most viewed product ");
+		btnOGetThe.setBounds(28, 154, 272, 23);
 		frame.getContentPane().add(btnOGetThe);
 		
-		txtGetstorename = new JTextField();
-		txtGetstorename.setText("Store Name");
-		txtGetstorename.setBounds(320, 12, 119, 20);
+		txtGetstorename = new JTextField();		
+		txtGetstorename.setBounds(320, 12, 143, 20);
 		frame.getContentPane().add(txtGetstorename);
 		txtGetstorename.setColumns(10);
 		
+		JComboBox<String> comboNumViews = new JComboBox<String>();
+		comboNumViews.setBounds(320, 104, 143, 20);
+		frame.getContentPane().add(comboNumViews);
+		for(int i=0;i<sotreController.getCurrentUser().getStoresName().size();i++){
+			comboNumViews.addItem(sotreController.getCurrentUser().getStoresName().get(i));
+		
+		}
+		JComboBox<String> comboMostViewed = new JComboBox<String>();
+		comboMostViewed.setBounds(320, 155, 143, 20);
+		frame.getContentPane().add(comboMostViewed);
+		for(int i=0;i<sotreController.getCurrentUser().getStoresName().size();i++){
+			comboMostViewed.addItem(sotreController.getCurrentUser().getStoresName().get(i));
+		
+		}
 	}
 }
